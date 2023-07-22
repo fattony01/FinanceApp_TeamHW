c6e45a5003a7e535b3d9c97ceefd9de2b0614da9
fattony <a.kondratenko.dev@gmail.com>
2023-07-23 00:18:47 +0200

    start

---
+++ FinanceApp_TeamHW/FinanceApp_TeamHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinanceApp_TeamHW
+//
+//  Created by Anton Kondratenko on 23.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
