04ef81afebd94d069a3a578ea687cee3cc0efaa8
Artem Potemkin <art.potemkin@gmail.com>
2023-07-31 19:39:40 +0300

    add transfer screen

--- FinanceApp_TeamHW/FinanceApp_TeamHW/ViewControllers/BankAccountsVC/AccountsTransferViewController.swift
+++ FinanceApp_TeamHW/FinanceApp_TeamHW/ViewControllers/BankAccountsVC/AccountsTransferViewController.swift
@@ -7,23 +7,43 @@
 
 import UIKit
 
-class AccountsTransferViewController: UIViewController {
-
+final class AccountsTransferViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+  
+    @IBOutlet var fromAccountPickerView: UIPickerView!
+    @IBOutlet var toAcountPickerView: UIPickerView!
+    
+    private let accounts = ["Alfa-Card", "Tinkoff-Card", "Cash", "Sberbank"]
+    private let secondAccounts = ["babau", "kakao", "chikau"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        fromAccountPickerView.dataSource = self
+        fromAccountPickerView.delegate = self
+        toAcountPickerView.dataSource = self
+        toAcountPickerView.delegate = self
+        
+        fromAccountPickerView.tag = 1
+        toAcountPickerView.tag = 2
+        
+    }
 
-        // Do any additional setup after loading the view.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch pickerView.tag {
+        case 1:
+            return accounts.count
+        default:
+            return secondAccounts.count
+        }
     }
-    */
-
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch pickerView.tag {
+        case 1: return accounts[row]
+        default: return secondAccounts[row]
+        }
+    }
 }
